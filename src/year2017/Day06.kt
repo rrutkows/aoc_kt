6bc1c7ac4ca8da3f05c96feb9a19c6ca5e80ac27
Rafał Rutkowski <rafal.rutkowski@hexagon.com>
2024-11-25 13:55:12 +0100

    y2017 d06

---
+++ src/year2017/Day06.kt
@@ -0,0 +1,45 @@
+package year2017
+
+import println
+import readInput
+
+private fun List<Int>.indexOfMax(): Int {
+    var found = -1
+    var foundVal = 0
+    forEachIndexed { i, v ->
+        if (found < 0 || v > foundVal) {
+            foundVal = v
+            found = i
+        }
+    }
+
+    return found
+}
+
+fun main() {
+    fun part1And2(input: String): Pair<Int, Int> {
+        var banks = input.split('\t').map { it.toInt() }
+        val s = banks.size
+        val history = mutableMapOf(banks to 0)
+        var cycles = 0
+        while (true) {
+            cycles += 1
+            val iMax = banks.indexOfMax()
+            val max = banks[iMax]
+            banks = banks.mapIndexed { i, v ->
+                val before = if (i == iMax) 0 else v
+                val distFromMax = if (i == iMax) s else (s + i - iMax) % s
+                before + (max / s) + if (distFromMax <= max % s) 1 else 0
+            }
+
+            if (banks in history) {
+                return cycles to cycles - history[banks]!!
+            }
+
+            history[banks] = cycles
+        }
+    }
+
+    val input = readInput(2017, 6).first()
+    part1And2(input).println()
+}
