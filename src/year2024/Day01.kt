7bea1149e1701004412ced895c0270c59203e800
Rafał Rutkowski <rafal.rutkowski@hexagon.com>
2024-12-01 18:04:09 +0100

    y2024 d01: groupingBy

--- src/year2024/Day01.kt
+++ src/year2024/Day01.kt
@@ -14,13 +14,11 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val (l1, l2) = input
+        return input
             .map { line -> line.split("   ").map { it.toInt() }.let { it[0] to it[1] } }
             .unzip()
-        val counts = mutableMapOf<Int, Int>()
-        l2.forEach { counts[it] = 1 + counts.getOrDefault(it, 0) }
-
-        return l1.sumOf { it * counts.getOrDefault(it, 0) }
+            .run { first to second.groupingBy { it }.eachCount() }
+            .run { first.sumOf { it * second.getOrDefault(it, 0) } }
     }
 
     val input = readInput(2024, 1)
