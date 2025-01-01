d510dd8b877cc07ca666fd0e818b8beda46f8e68
Rafał Rutkowski <rrutkowski@gmail.com>
2025-01-01 15:54:33 +0100

    y2024 d22: optimized

--- src/year2024/Day22.kt
+++ src/year2024/Day22.kt
@@ -20,27 +20,39 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val allSequences = mutableMapOf<List<Int>, Int>().withDefault { 0 }
+        // A sequence will be converted from a list if four -9 to 9 numbers
+        // to a list of four 0 to 19 numbers
+        // and packed into a 20 bits integer.
+        // Prices got from each possible sequence will be stored in the following vector instead of a hashmap.
+        val allSequences = MutableList(1 shl 20) { 0 }
+        val sequenceMask = (1 shl 20) - 1
         input.forEach { line ->
             var prev = line.toLong()
-            val seen = mutableSetOf<List<Int>>()
-            (0..<2000)
-                .asSequence()
-                .map { next(prev).also { prev = it } }
-                .map { (it % 10).toInt() }
-                .windowed(5)
-                .forEach { prices ->
-                    val sequence = prices
-                        .zipWithNext()
-                        .map { it.second - it.first }
-                    if (sequence !in seen) {
-                        allSequences[sequence] = allSequences.getValue(sequence) + prices.last()
-                        seen.add(sequence)
+            var sequence = 0
+
+            // Store sequences seen by a single seller in a bitmap instead of a hashmap.
+            // Round the size of the bitmap up to the nearest multiple of 64.
+            val seen = MutableList(((1 shl 20) + ULong.SIZE_BITS - 1) / ULong.SIZE_BITS) { 0UL }
+            for (i in 0..<2000) {
+                val number = next(prev)
+                val price = (number % 10).toInt()
+                val priceDiff = 9 + price - (prev % 10).toInt()
+                sequence = ((sequence shl 5) + priceDiff) and sequenceMask
+
+                // We don't get a full sequence until seeing at least 4 new numbers.
+                if (i >= 3) {
+                    val wordIndex = sequence / ULong.SIZE_BITS
+                    val bitIndex = sequence % ULong.SIZE_BITS
+                    if (seen[wordIndex] and (1UL shl bitIndex) == 0UL) {
+                        allSequences[sequence] += price
+                        seen[wordIndex] = seen[wordIndex] or (1UL shl bitIndex)
                     }
                 }
+                prev = number
+            }
         }
 
-        return allSequences.values.max()
+        return allSequences.max()
     }
 
     val input = readInput(2024, 22)
